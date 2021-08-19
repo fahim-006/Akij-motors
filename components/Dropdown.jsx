01844d006362deb7d89968eb7ff2bc0706e3eb37
Mashrur Hossain Khan <mashrurhossainkhan@Mashrurs-MacBook-Pro.local>
2021-08-19 21:07:56 +0600

    header bottom5

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -5,26 +5,21 @@ import classes from './Dropdown.module.scss';;
 import SubMenu from './HeroSection/Sidebar/SubMenu';
 
 function Dropdown() {
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
-
   return (
     <>
-      <ul
-        style={{}}
-        onClick={handleClick}
-        className={classes.click ? classes.dropdownamenuclicked : classes.dropdownamenu}>
+    <div
+       style={{height: "508px"}} 
+        className={classes.dropdownamenu}>
         {sideBarData.map((item, index) => {
           return (
-            <li  key={index} >
+          
              
                 <SubMenu item={item} key={index} />
              
-            </li>
+            
           );
         })}
-      </ul>
+      </div>
     </>
   );
 }
--- components/HeroSection/Sidebar/Sidebar.jsx
+++ components/HeroSection/Sidebar/Sidebar.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react';
-import Link from 'next/link';
-// import { IoIosArrowForward, IoIosArrowDown } from 'react-icons/io';
 import { sideBarData } from '../../../sidebardata';
-import classes from './Sidebar.module.scss';
 import SubMenu from './SubMenu';
 
-const Sidebar = () => {
-  
+const Sidebar = () => { 
   return (
-    <div>
+    <div>      
       {sideBarData.map((menu, index) => (
-     
-            <SubMenu item={menu} key={index} /> 
-        
-                     
-       
+              <SubMenu item={menu} key={index} />        
       ))}
     </div>
   );
